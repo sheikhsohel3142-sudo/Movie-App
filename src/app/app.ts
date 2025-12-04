2cc6c03b520536ae0d9092f14a963d5c58820aee
unknown <sheikhsohel3142@gmail.com>
2025-12-04 19:08:27 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Movie } from './services/movie';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,36 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('Movie-Web-App');
+  movieName:any;
+  a:any;
+  constructor(private movie:Movie){}
+  inputValue(event:Event){
+    this.a = (event.target as HTMLInputElement).value;
+    
+         this.movie.getMovieByTitle(this.a).subscribe((res:any)=>
+    { 
+      this.movieName=res;
+      
+
+      console.log(res);
+    })
+
+  }
+  getMovie(){
+        this.movie.getMovieByTitle(this.a).subscribe((res:any)=>
+    { 
+      this.movieName=res;
+      
+
+      console.log(res);
+    })
+  
 }
+}
+
+    
+ 
+  
+
+
+
---
+++ src/app/services/movie.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Movie {
+
+apiUrl = 'https://www.omdbapi.com/?apikey=4fdbf800';
+
+constructor(private http: HttpClient) {}
+
+getMovieByTitle(title: string): Observable<any> {
+  return this.http.get(`${this.apiUrl}&t=${title}`);
+}
+
+
+  
+}
